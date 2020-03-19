cb2034696b8529938306d952f09490ddf2cd8e26
Graham McIntire <gmcintire@getaroom.com>
2020-03-19 15:24:08 -0500

    update some deps

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-export XGPS_PORT="ttyUSB0"
+export XGPS_PORT="/dev/cu.usbserial-210"
 export XGPS_DRIVER="GP04S"
--- mix.exs
+++ mix.exs
@@ -2,28 +2,31 @@ defmodule XGPS.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :xgps,
-     name: XGPS,
-     version: "0.4.1",
-     elixir: ">= 1.3.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     source_url: "https://github.com/royveshovda/xgps",
-     deps: deps(),
-     description: description(),
-     package: package()]
+    [
+      app: :xgps,
+      name: XGPS,
+      version: "0.4.1",
+      elixir: ">= 1.7.0",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/gmcintire/xgps",
+      deps: deps(),
+      description: description(),
+      package: package()
+    ]
   end
 
   def application do
-    [applications: [:logger, :gen_stage],
-     mod: {XGPS, []}]
+    [applications: [:logger, :gen_stage], mod: {XGPS, []}]
   end
 
   defp deps do
-    [{:circuits_uart, "~> 1.3"},
-     {:mix_test_watch, "~> 0.9"},
-     {:gen_stage, "~> 0.14"},
-     {:ex_doc, ">= 0.0.0", only: :dev}
+    [
+      {:circuits_uart, "~> 1.3"},
+      {:mix_test_watch, "~> 0.9"},
+      {:gen_stage, "~> 0.14"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:credo, "~> 1.3", only: [:dev, :test]}
     ]
   end
 
@@ -36,11 +39,13 @@ defmodule XGPS.Mixfile do
   end
 
   defp package do
-    [# These are the default files included in the package
-     name: :xgps,
-     files: ["lib", "mix.exs", "README*", "LICENSE*", "simulator_positions.txt"],
-     maintainers: ["Roy Veshovda"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/royveshovda/xgps"}]
+    # These are the default files included in the package
+    [
+      name: :xgps,
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "simulator_positions.txt"],
+      maintainers: ["Roy Veshovda"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/royveshovda/xgps"}
+    ]
   end
 end
